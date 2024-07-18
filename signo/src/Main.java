0333c3238faa1f6758311d9962515c9420c30259
la1ni <marilainarasilva@hotmail.com>
2024-07-18 19:44:05 -0300

    Atualizado exercício Signo

--- signo/src/Main.java
+++ signo/src/Main.java
@@ -5,22 +5,22 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite o número correspondente ao seu mês de nascimento: ");
-        int mesInserido = scanner.nextInt();
+        int mes = scanner.nextInt();
 
         System.out.print("Digite o dia em que você nasceu: ");
         int diaNascimento =  scanner.nextInt();
 
-        String resposta = (mesInserido == 3 && diaNascimento >= 21) || (mesInserido == 4 && diaNascimento <= 20) ? "Áries":
-                (mesInserido == 4 && diaNascimento >= 21) || (mesInserido == 5 && diaNascimento <= 20) ? "Touro":
-                (mesInserido == 5 && diaNascimento >= 21) || (mesInserido == 6 && diaNascimento <= 20) ? "Gêmeos":
-                (mesInserido == 6 && diaNascimento >= 21) || (mesInserido == 7 && diaNascimento <= 22) ? "Câncer":
-                (mesInserido == 7 && diaNascimento >= 23) || (mesInserido == 8 && diaNascimento <= 22) ? "Leão":
-                (mesInserido == 8 && diaNascimento >= 23) || (mesInserido == 9 && diaNascimento <= 22) ? "Virgem":
-                (mesInserido == 9 && diaNascimento >= 23) || (mesInserido == 10 && diaNascimento <= 22) ? "Libra":
-                (mesInserido == 10 && diaNascimento >= 23) || (mesInserido == 11 && diaNascimento <= 21) ? "Escorpião":
-                (mesInserido == 11 && diaNascimento >= 22) || (mesInserido == 12 && diaNascimento <= 21) ? "Sagitário":
-                (mesInserido == 12 && diaNascimento >= 22) || (mesInserido == 1 && diaNascimento <= 20) ? "Capricórnio":
-                (mesInserido == 1 && diaNascimento >= 21) || (mesInserido == 2 && diaNascimento <= 18) ? "Aquário":
+        String resposta = (mes == 3 && diaNascimento >= 21) || (mes == 4 && diaNascimento <= 20) ? "Áries":
+                (mes == 4 && diaNascimento >= 21) || (mes == 5 && diaNascimento <= 20) ? "Touro":
+                (mes == 5 && diaNascimento >= 21) || (mes == 6 && diaNascimento <= 20) ? "Gêmeos":
+                (mes == 6 && diaNascimento >= 21) || (mes == 7 && diaNascimento <= 22) ? "Câncer":
+                (mes == 7 && diaNascimento >= 23) || (mes == 8 && diaNascimento <= 22) ? "Leão":
+                (mes == 8 && diaNascimento >= 23) || (mes == 9 && diaNascimento <= 22) ? "Virgem":
+                (mes == 9 && diaNascimento >= 23) || (mes == 10 && diaNascimento <= 22) ? "Libra":
+                (mes == 10 && diaNascimento >= 23) || (mes == 11 && diaNascimento <= 21) ? "Escorpião":
+                (mes == 11 && diaNascimento >= 22) || (mes == 12 && diaNascimento <= 21) ? "Sagitário":
+                (mes == 12 && diaNascimento >= 22) || (mes == 1 && diaNascimento <= 20) ? "Capricórnio":
+                (mes == 1 && diaNascimento >= 21) || (mes == 2 && diaNascimento <= 18) ? "Aquário":
                 "Peixes";
 
         scanner.close();
