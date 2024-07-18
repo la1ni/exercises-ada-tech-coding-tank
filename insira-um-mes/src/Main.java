061c9212295c2f5404c676001f98198d3e7cdd1f
la1ni <marilainarasilva@hotmail.com>
2024-07-18 19:44:19 -0300

    Atualizado exercício Insira um Mês

--- insira-um-mes/src/Main.java
+++ insira-um-mes/src/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite um número de 1 a 12: ");
-        int mesInserido = scanner.nextInt();
+        int mes = scanner.nextInt();
 
-        String resposta = (mesInserido == 1) ? "Janeiro": (mesInserido == 2) ? "Fevereiro": (mesInserido == 3) ? "Março": (mesInserido == 4) ? "Abril": (mesInserido == 5) ? "Maio": (mesInserido == 6) ? "Junho": (mesInserido == 7) ? "Julho": (mesInserido == 8) ? "Agosto":(mesInserido == 9) ? "Setembro": (mesInserido == 10) ? "Outubro": (mesInserido == 11) ? "Novembro": "Dezembro";
+        String resposta = (mes == 1) ? "Janeiro": (mes == 2) ? "Fevereiro": (mes == 3) ? "Março": (mes == 4) ? "Abril": (mes == 5) ? "Maio": (mes == 6) ? "Junho": (mes == 7) ? "Julho": (mes == 8) ? "Agosto":(mes == 9) ? "Setembro": (mes == 10) ? "Outubro": (mes == 11) ? "Novembro": "Dezembro";
 
         scanner.close();
 
